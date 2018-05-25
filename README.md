feec36ab428d9cd493eaddb8410161c447a41b2d
Chris Davenport <chris@christopherdavenport.tech>
2018-05-25 12:59:54 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# docker-postgresql-db-with-roles
+
+New Environmental
+
+## `POSTGRES_ROLE`
+
+Environmental Variable That Delegates a Role to create.
+Give that role to `POSTGRES_USER` and delegates
+all permissions on `POSTGRES_DB`.
---
+++ create-postgresql-db-with-role.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+set -u
+
+function create_role_in_db() {
+  local database=$1
+  local user=$2
+  local role=$3
+  echo "Creating role '$role' and database '$database'"
+  psql -v ON_ERROR_STOP=1 --username "$user" -d "$database" <<-EOSQL
+    CREATE ROLE "$role";
+EOSQL
+  echo "Granting role '$role' to user '$user'"
+  psql -v ON_ERROR_STOP=1 --username "$user" -d "$database" <<-EOSQL
+    GRANT "$role" TO "$user";
+EOSQL
+  echo "Granting all privileges on database '$database' to role '$role'"
+  psql -v ON_ERROR_STOP=1 --username "$user" -d "$database" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$database" TO "$role";
+EOSQL
+}
+
+create_role_in_db $POSTGRES_DB $POSTGRES_USER $POSTGRES_ROLE
